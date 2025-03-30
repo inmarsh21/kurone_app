5c1cd326d36c4c37671d6c95cbd241866fdfacf4
inmarsh21 <inmarsh21@yahoo.co.jp>
2025-03-30 20:51:11 +0900

    1

--- router.py
+++ router.py
@@ -6,8 +6,12 @@ from utils.keywords import fortune_keywords, worry_keywords
 def route_message(user_id, user_message):
     message = user_message.lower()
 
+    from responses import match
+
     # 占いメニュー番号 or 名称で分岐
     if message in ["1", "相性", "相性診断"]:
+        return match.run_match_fortune(user_id, message)
+    if user_id in match.session_state:
         return match.run_match_fortune(user_id, message)
 
     elif message in ["2", "タロット"]:
