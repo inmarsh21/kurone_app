f6bfed4add4399c2518a24f563fda6197321ad36
inmarsh21 <inmarsh21@yahoo.co.jp>
2025-04-02 01:16:53 +0900

    6

--- router.py
+++ router.py
@@ -18,7 +18,7 @@ def route_message(user_id, user_message):
         return base.reply_tarot()
 
     elif message in ["3", "ラッキーカラー", "カラー", "色"]:
-        return color.run_color_fortune(user_id, message)
+        base.reply_color()
 
     # 占いワードに該当
     elif any(keyword in message for keyword in fortune_keywords):
